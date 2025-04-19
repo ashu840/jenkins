7099207a730d188b8180dfbb06294107e43ed976
Ashutosh Dwivedi <ashutoshdwivediashu98@gmail.com>
2025-04-19 11:45:32 +0530

    second commit

---
+++ JenkinsProject/src/test/java/demo/sample.java
@@ -0,0 +1,18 @@
+package demo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class sample {
+@Test
+	
+	public void lunch1() {
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://demowebshop.tricentis.com/");
+		Reporter.log("Demowebshop launched twice Successfully",true);
+	}
+
+
+}
