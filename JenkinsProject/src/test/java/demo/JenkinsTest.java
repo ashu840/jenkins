9536d6bc5ce482c9c38fc36ba329120292dca304
Ashutosh Dwivedi <ashutoshdwivediashu98@gmail.com>
2025-04-19 10:47:00 +0530

    first commit

---
+++ JenkinsProject/src/test/java/demo/JenkinsTest.java
@@ -0,0 +1,18 @@
+package demo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class JenkinsTest {
+	
+	@Test
+	
+	public void lunch() {
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://demowebshop.tricentis.com/");
+		Reporter.log("Demowebshop launched Successfully",true);
+	}
+
+}
