0af5cf2e57f9db41a00a686ed157811dbe5da9ed
Ashutosh Dwivedi <ashutoshdwivediashu98@gmail.com>
2025-04-21 10:46:48 +0530

    PollSCM added

---
+++ JenkinsProject/src/test/java/demo/DemoTest.java
@@ -0,0 +1,19 @@
+package demo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class DemoTest {
+	
+@Test
+	
+	public void lunch() {
+		WebDriver driver=new ChromeDriver();
+		
+		Reporter.log("Poll SCM added",true);
+	}
+
+
+}
